b085087c08b7f4a4646a041b4bf42c752d4ae346
Carolina <carolina.schillemans@gmail.com>
2023-03-24 15:42:49 +0100

    con appCo;ponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>
-    Welcome to {{ title }}!
-  </h1>
-  <app-input-button-unit></app-input-button-unit>
-  <app-input-button-unit></app-input-button-unit>
-  <app-input-button-unit></app-input-button-unit>
-`,
+    template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+
+    <app-list-manager></app-list-manager>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = 'AppLevel';
+  title = 'My To Do List APP';
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { ListManagerComponent } from './list-manager/list-manager.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonUnitComponent
+    InputButtonUnitComponent,
+    ListManagerComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
